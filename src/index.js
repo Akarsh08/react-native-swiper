c658119e030b639045a40c12da6c865811381f8c
Akarsh Srivastava <akarsh@Akarshs-MacBook-Pro.local>
2020-02-03 19:07:47 +0530

    fixed: viewpager android package

--- src/index.js
+++ src/index.js
@@ -11,10 +11,11 @@ import {
   ScrollView,
   Dimensions,
   TouchableOpacity,
-  ViewPagerAndroid,
   Platform,
   ActivityIndicator
 } from 'react-native'
+
+import ViewPagerAndroid from '@react-native-community/viewpager';
 
 const { width, height } = Dimensions.get('window')
 
