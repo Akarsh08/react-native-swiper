a9a92b365f6d98a921ccc40e7bc751871a40d04a
Akarsh Srivastava <akarsh@Akarshs-MacBook-Pro.local>
2020-02-06 20:04:51 +0530

    fixed: ios index mess and android flicker on loop reset

--- src/index.js
+++ src/index.js
@@ -197,12 +197,9 @@ export default class extends Component {
   loopJumpTimer = null
 
   UNSAFE_componentWillReceiveProps(nextProps) {
-    if (!nextProps.autoplay && this.autoplayTimer)
+    if (!nextProps.autoplay && this.autoplayTimer){
       clearTimeout(this.autoplayTimer)
-    if (nextProps.index === this.props.index) return
-    this.setState(
-      this.initState(nextProps, this.props.index !== nextProps.index)
-    )
+    }
   }
 
   componentDidMount() {
@@ -224,15 +221,6 @@ export default class extends Component {
     // If autoplay props updated to true, autoplay immediately
     if (this.props.autoplay && !prevProps.autoplay) {
       this.autoplay()
-    }
-    if (this.props.children !== prevProps.children) {
-      if (this.props.loadMinimal && Platform.OS === 'ios') {
-        this.setState({ ...this.props, index: this.state.index })
-      } else {
-        this.setState(
-          this.initState({ ...this.props, index: this.state.index }, true)
-        )
-      }
     }
   }
 
